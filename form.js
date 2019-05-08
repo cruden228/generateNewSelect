10588a791b380084d572d54de6f7678a7e57d2df
Den <den.matvssnko.99@ukr.net>
2019-05-08 12:43:58 +0300

    upgrade)

--- form.js
+++ form.js
@@ -1,37 +1,43 @@
-const forEach = (array, iteratee) => {
-  	Array.prototype.forEach.call(array, iteratee);
-}
-
-const select = document.getElementById("example-select");
-select.style.display = "none";
-
-generateNewSelect(select);
-
-function generateNewSelect(oldSelect) {
-	const newSelect = document.createElement("div");
-	const newSelected = document.createElement("div");
-	const newOptions = document.createElement("ul");
-	newSelect.classList.add(oldSelect.classList.value);
-	newSelect.appendChild(newSelected);
-	newSelect.appendChild(newOptions);
-
-	forEach(oldSelect,(option, index) => {
-		const newOption = document.createElement("li");
-		const newOptionClass = option.classList.value;
-		if(newOptionClass) newOption.classList.add(newOptionClass);
-		newOption.innerHTML = option.innerHTML;
-		newOptions.appendChild(newOption);
-		if(option.hasAttribute("selected")){
-			newSelected.innerHTML = option.innerHTML;
+class Select {
+	constructor(object){
+		this.object = object;
+		this.forEach = (array, iteratee) => {
+		  	Array.prototype.forEach.call(array, iteratee);
 		}
-	});
-	oldSelect.parentElement.insertBefore(newSelect, oldSelect.nextElementSibling);
+		this.select = document.getElementById(object.el);
+	}
 
-	newSelect.addEventListener("click", (event)=>{
-		if(event.target.tagName !== "LI") return
-		newSelected.innerHTML = event.target.innerHTML;
-	}, false)
+	generateNewSelect() {
+		const oldSelect = this.select;
+		const newSelect = document.createElement("div");
+		const newSelected = document.createElement("div");
+		const newOptions = document.createElement("ul");
+		newSelect.classList.add(oldSelect.classList.value);
+		newSelect.appendChild(newSelected);
+		newSelect.appendChild(newOptions);
+
+		this.forEach(oldSelect.children,(option, index) => {
+			const newOption = document.createElement("li");
+			const newOptionClass = option.classList.value;
+			if(newOptionClass) newOption.classList.add(newOptionClass);
+			newOption.innerHTML = option.innerHTML;
+			newOptions.appendChild(newOption);
+			if(option.hasAttribute("selected")){
+				newSelected.innerHTML = option.innerHTML;
+			}
+		});
+
+		oldSelect.style.display = "none";
+		oldSelect.parentElement.insertBefore(newSelect, oldSelect.nextElementSibling);
+
+		newSelect.addEventListener("click", (event)=>{
+			if(event.target.tagName !== "LI") return
+			newSelected.innerHTML = event.target.innerHTML;
+		}, false)
+	}
 }
+
+new Select( { el: "example-select" } ).generateNewSelect();
 
 
 
